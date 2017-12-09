b3055b9ccc29dd9f3b75758c9982d63d800e6df4
Bharat Rele <rele.bharat@gmail.com>
2017-12-08 19:21:18 -0800

    Creating Put, Delete and Get routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,43 @@
+let ObjectID = require('mongodb').ObjectID;
 module.exports = (app, db) => {
+    
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if(err){
+                res.send({ 'error': 'An error occured in Get!' });
+            }else{
+                res.send(item);
+            }
+        });
+    });    
+        
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if(err){
+                res.send({ 'error': 'An error occured in Get!' });
+            }else{
+                res.send('Note ' + id + " deleted");
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };  
+        db.collection('notes').update(details, note, (err, item) => {
+            if(err){
+                res.send({ 'error': 'An error occured in Get!' });
+            }else{
+                res.send(item);
+            }
+        });
+    }); 
+
     app.post('/notes', (req, res) => {
         //we'll create the note here
         //console.log(req.body);
